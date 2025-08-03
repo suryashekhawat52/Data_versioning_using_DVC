2578b276069e6d8ad7be449256db7180fdec2654
SuryaShekhawat <suryashekhawat52@gmail.com>
2025-08-03 08:48:11 +0530

    first commit after dvc initialization

---
+++ pydata.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+
+# creating a sample dataset
+
+data = {'name': ['Surya','Ashish', 'Vikas'],
+      'age' : [26,25,28],
+      'rank' : [1,2,3]}
+
+df = pd.DataFrame(data)
+
+#creating new version of data
+
+
+
+#creating a folder and then making a directory
+
+myfolder = 'Profile'
+
+os.makedirs(myfolder, exist_ok = True)
+
+file_path = os.path.join(myfolder, 'samples.csv')
+
+#writing the data to the define file_path
+
+df.to_csv(file_path, index = False)
